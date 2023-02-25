d6142148259442b681589f81e64ce5884a75ce0c
TRIAEIOU <94647023+TRIAEIOU@users.noreply.github.com>
2023-02-25 21:14:35 +0100

    Add pure gfm table

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
-export { xTableHtml } from "./lib/html.js";
-export { xTable } from "./lib/syntax.js";
+export { xTableHtml } from './lib/html.js'
+export { gfmTableHtml } from './lib/gfm-html.js'
+export { xTable } from './lib/syntax.js'
+export { gfmTable } from './lib/gfm-syntax.js'
