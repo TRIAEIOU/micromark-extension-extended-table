ba430f717e32343298030f6a660b8fb81fc1db7c
TRIAEIOU <94647023+TRIAEIOU@users.noreply.github.com>
2023-02-25 22:32:19 +0100

    Fix

--- dev/index.js
+++ dev/index.js
@@ -1,2 +1,4 @@
-export {xtableHtml} from './lib/html.js'
-export {xtable} from './lib/syntax.js'
+export { xtableHtml } from './lib/html.js'
+export { gfmTableHtml } from './lib/gfm-html.js'
+export { xtable } from './lib/syntax.js'
+export { gfmTable } from './lib/gfm-syntax.js'
