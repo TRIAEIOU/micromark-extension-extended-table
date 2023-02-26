be6588ccb170d0bc4de223b6e6750368740a74fc
TRIAEIOU <94647023+TRIAEIOU@users.noreply.github.com>
2023-02-26 09:52:52 +0100

    Initial

---
+++ test/debug.js
@@ -0,0 +1,16 @@
+import test from 'tape'
+import {micromark} from 'micromark'
+import {gfmTable as syntax, gfmTableHtml as html} from '../dev/index.js'
+
+test('markdown -> html (micromark)', (t) => {
+  t.deepEqual(
+    micromark('|0|\n|-|\n|1|', {
+      extensions: [syntax],
+      htmlExtensions: [html]
+    }),
+    '<table>\n<thead>\n<tr>\n<th>a</th>\n</tr>\n</thead>\n<tbody>\n<tr>\n<td>b</td>\n</tr>\n</tbody>\n</table>',
+    'should support a table w/ a body row ending in an eof (1)'
+  )
+
+  t.end()
+})
